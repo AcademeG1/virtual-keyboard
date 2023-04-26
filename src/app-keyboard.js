2bb00db6f952597b134a5f4cdd753be322e99b74
Michail <36785100+AcademeG1@users.noreply.github.com>
2023-04-26 10:29:16 +0300

    feat: added position style key in keyboard and fix render keyboard logic

--- src/app-keyboard.js
+++ src/app-keyboard.js
@@ -2,6 +2,7 @@ import {keys} from './keys.js';
 import Header from './components/header/Header.js';
 import Keyboard from './components/keyboard/Keyboards.js';
 import Footer from './components/footer/Footer.js';
+import {keysRus} from './keysRus.js';
 
 const CSS_STYLES = {
   WRAPPER: 'wrapper',
@@ -14,7 +15,7 @@ const CSS_STYLES = {
 };
 
 const header = new Header('RSS Keyboard display', CSS_STYLES.HEADER_TITLE);
-const keyboard = new Keyboard(keys);
+const keyboard = new Keyboard(language == 'en' ? keys : keysRus, language);
 const footer = new Footer(
     ['The keyboard was created in the operating system macOS',
       'To switch the language combination: left control + space'],
---
+++ src/keysRus.js
@@ -0,0 +1,9 @@
+export const keysRus = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4',
+  'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal',
+  'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г',
+  'ш', 'щ', 'з', 'х', 'ъ', 'IntlBackslash', 'CapsLock',
+  'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д',
+  'ж', 'э', 'ё', 'Enter', 'ShiftLeft', 'я',
+  'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', 'Slash',
+  'ShiftRight', 'ControlLeft', 'AltLeft', 'MetaLeft', 'Space', 'MetaRight',
+  'AltRight', 'ArrowLeft', 'ArrowUp', 'ArrowDown', 'ArrowRight'];
